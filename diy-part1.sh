ba530233672240434fed65a185717fd0922fa72c
lsydy260806 <lsydy@189.cn>
2026-08-07 22:35:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,3 @@
 #sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 #sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 #echo 'src-git luci https://github.com/lsydy260806/luci-app-adguardhome.git;main' >>feeds.conf.default
-sed -i '1i src-git luci https://github.com/lsydy260806/luci-app-adguardhome.git;main' feeds.conf.default
