52857848eacf9a3661ef37864b9aa34946ee8fd7
lsydy260806 <lsydy@189.cn>
2026-08-07 15:54:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,12 +33,9 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
 #调整smartdns和adguardhome插件
 rm -rf feeds/luci/applications/luci-app-adguardhome
 git clone https://github.com/lsydy260806/luci-app-adguardhome feeds/luci/applications/luci-app-adguardhome
-
 rm -rf feeds/packages/net/adguardhome
 git clone https://github.com/lsydy260806/adguardhome feeds/packages/net/adguardhome
-
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/lsydy260806/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/lsydy260806/smartdns feeds/packages/net/smartdns
