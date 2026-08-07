18168203488ec1433475c52f867d3e210b751cc2
lsydy260806 <lsydy@189.cn>
2026-08-07 23:25:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 #echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
 #sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 #sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-echo 'src-git LEAN https://github.com/lsydy260806/luci-app-adguardhome.git;main' >>feeds.conf.default
+#echo 'src-git LEAN https://github.com/lsydy260806/luci-app-adguardhome.git;main' >>feeds.conf.default
